3026341bc1384299eb3d67d7bd25e0e5e421e6c5
LiGER <samarghandi112w34@gmail.com>
2025-09-17 18:49:56 +0330

    Added the color palette.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./App.css";
+import { useEffect } from "react";
+import { handleAddWatches } from "./db/handleAddData";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
+  useEffect(() => {
+    const insertData = async () => {
+      try {
+        const data = await handleAddWatches();
+        console.log("incerted data", data);
+      } catch (error) {
+        console.log("error:", error);
+      }
+    };
+    insertData();
+  }, []);
+
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: <h1>home</h1>,
-    },
-    {
-      path: "/pricing",
-      element: <h1>pricing</h1>,
-    },
-    {
-      path: "*",
-      element: <h1>page not found :(</h1>,
+      element: <Navbar />,
+      children: [
+        {
+          path: "/",
+          element: <h1>home</h1>,
+        },
+        {
+          path: "/pricing",
+          element: <h1>pricing</h1>,
+        },
+        {
+          path: "*",
+          element: <h1>page not found :(</h1>,
+        },
+      ],
     },
   ]);
   return <RouterProvider router={router} />;
