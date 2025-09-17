f0ce4650b8f65d88f8eb38648e0fdc3993805956
LiGER <samarghandi112w34@gmail.com>
2025-09-17 17:37:27 +0330

    Added more fields to the User interface for rich details

--- src/types/watch.ts
+++ src/types/watch.ts
@@ -3,14 +3,25 @@ export interface Watch {
   brand: string;
   model: string;
   price: number;
+
+  case_diameter: number;
+  case_thickness: number;
+  case_material: string;
+
   strap_material:
-    | "steel"
-    | "leather"
-    | "rubber"
-    | "fabric"
-    | "synthetic"
-    | "resin";
+  | "steel"
+  | "leather"
+  | "rubber"
+  | "fabric"
+  | "synthetic"
+  | "resin"
+  | "gold"
+  | "titanium"
+  | "alligator";
+
   display_type: "analog" | "digital" | "analog-digital";
+  movement_type: "automatic" | "manual" | "quartz" | "solar" | "kinetic";
+
   tags: string[];
   images: string[];
   quantity: number;
